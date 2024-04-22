4280209e5d6735968bbce8c766eb2b4f25bd409e
barab <94920370+Baraban511@users.noreply.github.com>
2024-04-22 13:32:52 +0200

    Rajout du css

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,6 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_404 from "./routes/_404.tsx";
+import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
@@ -10,6 +11,7 @@ import { type Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
+    "./routes/_app.tsx": $_app,
     "./routes/index.tsx": $index,
   },
   islands: {},
---
+++ routes/_app.tsx
@@ -0,0 +1,22 @@
+import { PageProps } from "$fresh/server.ts";
+import Header from "../components/header.tsx"
+import Footer from "../components/footer.tsx";
+
+export default function App({ Component }: PageProps) {
+  return (
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="/styles.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>BarabSite</title>
+      </head>
+      <body>
+        <Header />
+        <Component />
+        <Footer />
+
+      </body>
+    </html>
+  );
+}
