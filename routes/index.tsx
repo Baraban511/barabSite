8c56c8035459835ae1511ba2c7c61a48e843dca3
barab <94920370+Baraban511@users.noreply.github.com>
2024-04-22 13:41:34 +0200

    Ajout des projets

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,10 @@
 import { useSignal } from "@preact/signals";
+import Projects from '../components/projects.tsx'
 
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 mx-auto bg-[#3fec5f]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
@@ -18,6 +19,7 @@ export default function Home() {
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
         </p>
       </div>
+    <Projects />
     </div>
   );
 }
