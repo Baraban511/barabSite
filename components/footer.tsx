a530379793e18a1b897be8566921e9c6b15684cb
barab <94920370+Baraban511@users.noreply.github.com>
2024-07-18 20:33:45 +0200

    Add github sponsors button

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,16 +10,14 @@ import IconXboxX from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/xbox-x.tsx
 import IconXboxY from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/xbox-y.tsx";
 export default function Footer() {
   const menu = {
-      title: "Randoms links",
-      children: [
-        { name: "EDC", href: "https://edc.barab.live" },
-        { name: "EDC - old", href: "https://barab.ovh" },
-        { name: "CO2 internship", href: "https://data.barab.live" },
-        { name: "Sponsor", href: "https://github.com/sponsors/Baraban511" },
-        { name: "Mail", href: "mailto:contact@barab.live?subject=Contact" },
-      ],
-    };
-
+    title: "Randoms links",
+    children: [
+      { name: "EDC", href: "https://edc.barab.live" },
+      { name: "EDC - old", href: "https://barab.ovh" },
+      { name: "CO2 internship", href: "https://data.barab.live" },
+      { name: "Mail", href: "mailto:contact@barab.live?subject=Contact" },
+    ],
+  };
   return (
     <div class="bg-[#fbffee] flex flex-col md:flex-row w-full max-w-screen gap-8 md:gap-16 px-8 text-sm pt-8">
       <div class="flex-1">
@@ -33,11 +31,11 @@ export default function Footer() {
         <div class="text-gray-500 mb-2 select-all">
           Learning developer
         </div>
-          <img
-            src="https://wakatime.com/badge/user/45f0ba86-46c8-41e2-8b47-b13184fb2b11/project/9335871f-f383-4abe-acf3-65e6092ebdd3.svg"
-            alt="wakatime"
-            class="h-6"
-          />
+        <img
+          src="https://wakatime.com/badge/user/45f0ba86-46c8-41e2-8b47-b13184fb2b11/project/9335871f-f383-4abe-acf3-65e6092ebdd3.svg"
+          alt="wakatime"
+          class="h-6"
+        />
         <a href="https://fresh.deno.dev">
           <img
             src="https://fresh.deno.dev/fresh-badge.svg"
@@ -48,34 +46,34 @@ export default function Footer() {
         </a>
       </div>
 
-        <div class="mb-4">
-          <div class="font-bold">{menu.title}</div>
-          <ul class="mt-2">
-            {menu.children.map((child) => (
-              <li class="mt-2" key={child.name}>
-                <a
-                  class="text-gray-500 hover:text-[#008080]"
-                  href={child.href}
-                >
-                  {child.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div class="mb-4">
+        <div class="font-bold">{menu.title}</div>
+        <ul class="mt-2">
+          {menu.children.map((child) => (
+            <li class="mt-2" key={child.name}>
+              <a
+                class="text-gray-500 hover:text-[#008080]"
+                href={child.href}
+              >
+                {child.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div class="text-gray-500 space-y-2">
         <div class="text-xs">
           Copyright © 2024 barab<br />
           All right reserved.
         </div>
-        <a
+
+        <div class='flex gap-3 items-center justify-center'><a
           href="https://github.com/Baraban511"
           class="inline-block hover:text-[#008080]"
           aria-label="GitHub"
-        >
-          <BrandGithub aria-hidden="true" />
-        </a>
+        ><BrandGithub aria-hidden="true" /></a><iframe src="https://github.com/sponsors/Baraban511/button" title="Sponsor Baraban511" height="32" width="114" style="border: 0; border-radius: 6px;"></iframe></div>
+
       </div>
       <div class="text-gray-500 space-y-2">
         <IconXboxA class="hover:text-[#008080]" />
