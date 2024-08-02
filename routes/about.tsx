a7463b0aa51586d08d71e3d22c584bb4ee303eff
barab <94920370+Baraban511@users.noreply.github.com>
2024-08-02 19:38:12 +0200

    Start about developement

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,5 +1,25 @@
-import Hero from "../components/hero.tsx";
 
-export default function About() {
-  return (<Hero />);//En cours
+import { Head } from "$fresh/runtime.ts";
+
+export default function VR() {
+  return (
+    <>
+      <Head>
+        <script src="https://aframe.io/releases/1.6.0/aframe.min.js"></script>
+      </Head>
+      <a-scene>
+        <a-box
+          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Waving%20Hand.png"
+          animation="property: rotate; to: 2.2; dir: alternate; dur: 2000; loop: true"></a-box>
+        <a-plane
+          src=""
+          position="0 0 -4" rotation="0 0 0" width="4" height="4" text="value: Hello, A-Frame!; color: #BBB" material="opacity: 0.0; transparent: true">
+        </a-plane>
+        <a-sky color="#008080"></a-sky>
+
+      </a-scene>
+    </>
+  );
 }
+
+
