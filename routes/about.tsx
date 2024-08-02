339b934cb8f0c86b2113a32ea2eec113b1b8e6c3
barab <94920370+Baraban511@users.noreply.github.com>
2024-08-02 20:57:07 +0200

    Small update on about me

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,22 +1,25 @@
 
+//@ts-nocheck Using A-frame
 import { Head } from "$fresh/runtime.ts";
-
 export default function VR() {
+
   return (
     <>
       <Head>
         <script src="https://aframe.io/releases/1.6.0/aframe.min.js"></script>
       </Head>
+
       <a-scene>
-        <a-box
-          src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand%20gestures/Waving%20Hand.png"
-          animation="property: rotate; to: 2.2; dir: alternate; dur: 2000; loop: true"></a-box>
+        <a-sky color="#008080"></a-sky>
+        <a-plane
+          src="/xbox_logo.png"
+          position="6 0 -10" rotation="0 -90 0" width="4" height="4" transparent="true">
+        </a-plane>
         <a-plane
           src=""
-          position="0 0 -4" rotation="0 0 0" width="4" height="4" text="value: Hello, A-Frame!; color: #BBB" material="opacity: 0.0; transparent: true">
+          position="-6 0 -10" rotation="0 90 0" width="4" height="4" text="value: Hello, A-Frame!; color: #BBB" >
         </a-plane>
-        <a-sky color="#008080"></a-sky>
-
+        <a-entity position="4 5 -10" rotation="0 0 0" scale="10 10 10" text="value: Stuff I like; "></a-entity>
       </a-scene>
     </>
   );
